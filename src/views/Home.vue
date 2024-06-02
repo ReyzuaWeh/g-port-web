8d283f8b906e18ddfb4844d66d51aad7c83c2435
ReyzuaWeh <ginanjar.alfarizi07@gmail.com>
2024-06-02 15:19:08 +0700

    Justify Text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="bg-gray-100 min-h-screen flex items-center justify-center">
         <div class="p-6 max-w-xl mx-auto bg-slate-50 rounded-xl shadow-lg space-y-4">
             <h1 class="text-3xl md:text-4xl font-semibold text-gray-800">Welcome to My Portfolio</h1>
-            <p class="text-gray-600 md:text-lg leading-relaxed">
+            <p class="text-gray-600 md:text-lg leading-relaxed text-justify">
                 Hi there! I made this simple website to explain about myself and testing some of my programming skills.
             </p>
             <div class="flex justify-between items-end">
